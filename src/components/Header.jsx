cb3f9cdc980706c118d4010c570003607211f9b8
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-02-24 11:08:54 +0100

    modif header + ajout du scss header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import logo from "../assets/logo.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
-  return <div></div>;
+  return (
+    <nav>
+      <img src={logo} alt="Logo" />
+      <ul>
+        <li>
+          <Link to={"/"}>Accueil</Link>
+        </li>
+        <li>
+          <Link to={"/a-propos"}>A Propos</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
