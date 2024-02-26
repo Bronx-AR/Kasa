e2469ef9d9cddaefbbd48feeda46404068dd9fca
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-02-26 13:44:13 +0100

    Modif du Card, Ajout du scss Card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logements from "../data/logements.json";
 
 export default function Card() {
-  return <div></div>;
+  return (
+    <section className="cardsContainer">
+      {logements.map((location, index) => (
+        <Link
+          to={"/fiche-logement" + location.id}
+          key={index + "" + location.id}
+        >
+          <article>
+            <img src={location.cover} alt={location.title} />
+            <h2>{location.title}</h2>
+          </article>
+        </Link>
+      ))}
+    </section>
+  );
 }
