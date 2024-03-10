1a19ee9bb2da8c6d8381c5140660bfd7e19aa4a5
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-03-10 10:13:14 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import Banner from "../components/Banner";
 import Card from "../components/Card";
 import logements from "../data/logements.json";
 
 export default function Home() {
-  const location = useLocation();
-
   return (
     <main>
-      <Banner location={location} />
+      <Banner />
       <section className="cardsContainer">
         {logements.map((location, index) => (
           <Card location={location} key={index} />
