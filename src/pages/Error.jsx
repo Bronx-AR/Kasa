3d48690bdb9bd0c3b46e5a878a0fb93f0e44fd0e
Arnaud Quatrevaux <arnaudqpro@outlook.com>
2024-03-10 10:42:32 +0100

    Update Error (jsx & scss)

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,17 +1,11 @@
 import React from "react";
 
-function Error() {
+export default function Error() {
   return (
-    <div className="page__ctn error">
-      <h1 className="error__title">404</h1>
-      <p className="error__txt">
-        Oups! La page que vous demandez n'existe pas.
-      </p>
-      <a className="error__link" href="/">
-        Retourner sur la page d'accueil
-      </a>
+    <div className="error">
+      <h1>404</h1>
+      <p>Oups! La page que vous demandez n'existe pas.</p>
+      <a href="/">Retournez sur la page d'accueil</a>
     </div>
   );
 }
-
-export default Error;
